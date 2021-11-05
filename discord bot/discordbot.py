6de500c1f7e40944356d3d44f7d506e6d15ea13c
Kayransteelink <33291@ma-web.nl>
2021-11-05 14:31:11 +0100

    eerste

--- discord bot/discordbot.py
+++ discord bot/discordbot.py
@@ -7,5 +7,5 @@ async def on_ready():
     guild = client.guilds[0]
     print(guild.name, "is the name of the server")
     print(client.user, "has connected to the client")
-client.run("OTA2MTY2MTQzNzAwNTg2NTI3.YYUq_A.vc4aP3ycGgrkL2DBI3nZTw8t8zo")
+client.run("")
 
